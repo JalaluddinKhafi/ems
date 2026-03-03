f178878b3842acaf8c0874faec0c8395601733f4
JalaluddinKhafi <khafi98117@gmail.com>
2026-03-04 00:41:21 +0430

    project structure created

---
+++ src/main/java/com/dahishclub/emsspringboot/Model/Employee.java
@@ -0,0 +1,22 @@
+package com.dahishclub.emsspringboot.Model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+}
