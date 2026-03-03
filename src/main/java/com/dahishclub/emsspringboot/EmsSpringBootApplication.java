10b2c35c5f483a32b447faa0bddbdd1f07c8f1e9
JalaluddinKhafi <khafi98117@gmail.com>
2026-03-03 22:52:19 +0430

    pom updated

---
+++ src/main/java/com/dahishclub/emsspringboot/EmsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dahishclub.emsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsSpringBootApplication.class, args);
+	}
+
+}
